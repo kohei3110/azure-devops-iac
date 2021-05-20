bc0ce44764c5505fdeb943ae58612ee85d3ad05b
kohei3110 <kk.31108424@gmail.com>
2021-05-20 21:22:27 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Azure Pipelines IaC
+
+## Project 作成
+Azure DevOps でプロジェクトを作成。
+
+## master ブランチに Push
+
+```Powershell
+git add .
+git commit -m "first commit"
+git push -u origin master
+```
---
+++ scripts/readOutputs.ps1
@@ -0,0 +1,20 @@
+﻿foreach($arg in $args) {
+    Write-Output "Retrieved input: $arg"
+    $armOutputObj = $arg | ConvertFrom-Json
+
+    $armOutputObj.PSObject.Properties | ForEach-Object {
+        $type = ($_.value.type).ToLower()
+        $keyname = "Output_"+$_.name
+        $value = $_.value.value
+
+        if ($type -eq "securestring") {
+            Write-Output "##vso[task.setvariable variable=$keyname;isSecret=true]$value"
+            Write-Output "Added variable '$keyname' ('$type')"
+        } elseif ($type -eq "string") {
+            Write-Output "##vso[task.setvariable variable=$keyname]$value"
+            Write-Output "Added variable '$keyname' ('$type') with value '$value'"
+        } else {
+            Throw "Type '$type' is not supported for '$keyname'"
+        }
+    }
+}
